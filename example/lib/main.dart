a593193ac97665194fb4d062cd15781500835412
vetukuri <varmavetukuri1234@gmail.com>
2023-12-11 10:08:32 +0530

    changed  lowerCamelCase

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ElevatedButton(
               child: Text('Fetch All Data'),
               onPressed: () {
-                whistle_loop_events("onlcikmybutton");
+                whistleLoopEvents("onlcikmybutton");
 
               },
             ),
--- lib/whistleloop_pixel_fire.dart
+++ lib/whistleloop_pixel_fire.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:whistle_loop_sdk/whistle_loop_sdk.dart';
-void whistle_loop_events(String eventname) async{
+void whistleLoopEvents(String eventName) async{
 
-  String? device_id = await WhistleLoopSdk().initDeviceId();
-  String? package_name= await WhistleLoopSdk().getpac();
-  String? network_type= await WhistleLoopSdk().getConnectionType();
+  String? deviceId = await WhistleLoopSdk().initDeviceId();
+  String? packageName= await WhistleLoopSdk().getpac();
+  String? networkType= await WhistleLoopSdk().getConnectionType();
   String? ip= await WhistleLoopSdk().getIP();
-  String? user_agent= await WhistleLoopSdk().getPlatform();
-  String? clicked_time= await WhistleLoopSdk().getTime();
-  String? clicked_date= await WhistleLoopSdk().getDate();
-  String? platform_os= await WhistleLoopSdk().getDeviceType();
+  String? userAgent= await WhistleLoopSdk().getPlatform();
+  String? clickedTime= await WhistleLoopSdk().getTime();
+  String? clickedDate= await WhistleLoopSdk().getDate();
+  String? platformOs= await WhistleLoopSdk().getDeviceType();
 
-  var request = http.Request('POST', Uri.parse('http://13.232.216.75/whistle-follow-utils/sdk_test.php?device_id=$device_id&event_name=$eventname&package_name=$package_name&network_type=$network_type&ip=$ip&user_agent=$user_agent&clicked_time=$clicked_time&clicked_date=$clicked_date&platfom_os=$platform_os'));
+  var request = http.Request('POST', Uri.parse('http://13.232.216.75/whistle-follow-utils/sdk_test.php?device_id=$deviceId&event_name=$eventName&package_name=$packageName&network_type=$networkType&ip=$ip&user_agent=$userAgent&clicked_time=$clickedTime&clicked_date=$clickedDate&platfom_os=$platformOs'));
   http.StreamedResponse response = await request.send();
   if (response.statusCode == 200) {
     debugPrint("sfhdsuyfgvudsgfusdgvf");
